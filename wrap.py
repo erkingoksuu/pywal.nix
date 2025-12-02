133b030a2f0c3e79f8c3b109b366251a106229cd
Erkin Göksu <92168507+erkingoksuu@users.noreply.github.com>
2025-12-02 10:19:27 +0300

    Update wrap.py

--- wrap.py
+++ wrap.py
@@ -3,7 +3,6 @@ import sys
 from pywal.backends import (
     colorthief,
     colorz,
-    fast_colorthief,
     haishoku,
     schemer2,
     wal,
@@ -14,7 +13,6 @@ if __name__ == "__main__" and len(sys.argv) > 1:
         {
             "colorthief": colorthief.get,
             "colorz": colorz.get,
-            "fast_colorthief": fast_colorthief.get,
             "haishoku": haishoku.get,
             "schemer2": schemer2.get,
             "wal": wal.get,
